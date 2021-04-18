d6ecd530e226099c164a53dcfa5f170c5ddb61e7
Sabrina Lu <sabrinalu@Sabrinas-MacBook-Pro.local>
2021-04-17 23:29:13 -0500

    changed login background and added logo

--- src/Containers/Login/Login.js
+++ src/Containers/Login/Login.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+import {ReactComponent as ReactLogo} from './Logo.svg';
 import './Login.css';
 import { Button} from 'react-bootstrap';
 
 function Login() {
   return (
     <div className="Login">
+      
       <header className="Login-header">
+      <ReactLogo />
       <p className="LogInButton">
                         <Button
                             variant="primary"
