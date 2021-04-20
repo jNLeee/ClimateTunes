99072e2090b9a11f738eae8dbc039a3b37a6ad06
christinaaag <christina.gan5@tamu.edu>
2021-04-20 04:04:42 -0500

    login working, cannot redirect to dashboard

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
-require('dotenv').config({ path: 'variables.env' });
+require('dotenv').config({ path: 'variable.env' });
 
 const express = require('express');
+const request = require('request');
 const cors = require('cors');
 const Datastore = require('nedb');
 const cron = require('node-cron');
@@ -23,10 +24,33 @@ app.get('/callback', getAccessToken, (req, res, next) => {
     if (err) {
       next(err);
     } else {
-      res.redirect(`${clientUrl}/?authorized=true`);
+      res.redirect(`${clientUrl}/Dashboard`);
     }
   });
 });
+
+// app.get('/callback', function(req, res) {
+//   let code = req.query.code || null
+//   let authOptions = {
+//     url: 'https://accounts.spotify.com/api/token',
+//     form: {
+//       code: code,
+//       redirect_uri: process.env.REDIRECT_URI,
+//       grant_type: 'authorization_code'
+//     },
+//     headers: {
+//       'Authorization': 'Basic ' + (Buffer.from(
+//         process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET, "base64"
+//       ))
+//     },
+//     json: true
+//   }
+//   request.post(authOptions, function(error, response, body) {
+//     var access_token = body.access_token
+//     let uri = process.env.FRONTEND_URI || 'http://localhost:3000'
+//     res.redirect(uri + '/Dashboard?access_token=' + access_token)
+//   })
+// }) 
 
 app.set('port', process.env.PORT || 5000);
 const server = app.listen(app.get('port'), () => {
