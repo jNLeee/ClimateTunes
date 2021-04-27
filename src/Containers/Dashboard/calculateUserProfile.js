738cb1a171a35447b4709521111d5f56073889db
Sam <leesamuel000@gmail.com>
2021-04-27 02:58:40 -0500

    updates

--- src/Containers/Dashboard/calculateUserProfile.js
+++ src/Containers/Dashboard/calculateUserProfile.js
@@ -1,3 +1,4 @@
+import calcMatrix from "./calcMatrix";
 
 // get user's top 50 tracks                                                                          <-- currently not using this but kept it just in case
 async function getTopTracks(access_token) {
@@ -40,38 +41,6 @@ async function getAudioFeatures(access_token, tracksData) {
     const audioFeaturesArray = audioFeatures.audio_features;
 
     return audioFeaturesArray;
-}
-
-// given array of all audio features in json, make a 2d array with all values
-function calcMatrix(audioFeaturesArray) {
-    var userMatrix = [];
-    var currVector = [];
-    var currTrack = undefined;
-    var loudness = 0;
-    var tempo = 0;
-    //console.log(audioFeaturesArray);
-
-    for (var i = 0; i < Object.keys(audioFeaturesArray).length; i++) {
-        currTrack = audioFeaturesArray[i];
-        currVector.push(currTrack.acousticness);
-        currVector.push(currTrack.danceability);
-        currVector.push(currTrack.energy);
-        currVector.push(currTrack.instrumentalness);
-        currVector.push(currTrack.liveness);
-        loudness = currTrack.loudness / (-60.0);
-        currVector.push(loudness);
-        currVector.push(currTrack.speechiness);
-        tempo = 1.0 - (60.0 / currTrack.tempo);
-        currVector.push(tempo);
-        currVector.push(currTrack.valence);
-
-        userMatrix.push(currVector);
-        // reset currVector
-        currVector = [];
-    }
-    //console.log(userMatrix);
-
-    return userMatrix;
 }
 
 // calculate user vector by averaging matrix values
